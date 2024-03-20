cbc627b956db58bdb2ae4c9aad78ce5c86c4982e
John Paul <nil@exploit.rs>
2024-03-20 19:17:02 +0900

    init

---
+++ src/routes/Header.svelte
@@ -0,0 +1,180 @@
+<script>
+	import { page } from '$app/stores';
+	import logo from '$lib/images/lock-logo.svg';
+	import github from '$lib/images/github.svg';
+</script>
+
+<!-- <header>
+	<div class="corner">
+		<a href="https://kit.svelte.dev">
+			<img src={logo} alt="SvelteKit" />
+		</a>
+	</div>
+
+	<nav>
+		<svg viewBox="0 0 2 3" aria-hidden="true">
+			<path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
+		</svg>
+		<ul>
+			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+				<a href="/">Home</a>
+			</li>
+			<li aria-current={$page.url.pathname === '/about' ? 'page' : undefined}>
+				<a href="/about">About</a>
+			</li>
+			<li aria-current={$page.url.pathname.startsWith('/sverdle') ? 'page' : undefined}>
+				<a href="/sverdle">Sverdle</a>
+			</li>
+		</ul>
+		<svg viewBox="0 0 2 3" aria-hidden="true">
+			<path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
+		</svg>
+	</nav>
+
+	<div class="corner">
+		<a href="https://github.com/sveltejs/kit">
+			<img src={github} alt="GitHub" />
+		</a>
+	</div>
+</header>
+
+<style>
+	header {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	.corner {
+		width: 3em;
+		height: 3em;
+	}
+
+	.corner a {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 100%;
+		height: 100%;
+	}
+
+	.corner img {
+		width: 2em;
+		height: 2em;
+		object-fit: contain;
+	}
+
+	nav {
+		display: flex;
+		justify-content: center;
+		--background: rgba(255, 255, 255, 0.7);
+	}
+
+	svg {
+		width: 2em;
+		height: 3em;
+		display: block;
+	}
+
+	path {
+		fill: var(--background);
+	}
+
+	ul {
+		position: relative;
+		padding: 0;
+		margin: 0;
+		height: 3em;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		list-style: none;
+		background: var(--background);
+		background-size: contain;
+	}
+
+	li {
+		position: relative;
+		height: 100%;
+	}
+
+	li[aria-current='page']::before {
+		--size: 6px;
+		content: '';
+		width: 0;
+		height: 0;
+		position: absolute;
+		top: 0;
+		left: calc(50% - var(--size));
+		border: var(--size) solid transparent;
+		border-top: var(--size) solid var(--color-theme-1);
+	}
+
+	nav a {
+		display: flex;
+		height: 100%;
+		align-items: center;
+		padding: 0 0.5rem;
+		color: var(--color-text);
+		font-weight: 700;
+		font-size: 0.8rem;
+		text-transform: uppercase;
+		letter-spacing: 0.1em;
+		text-decoration: none;
+		transition: color 0.2s linear;
+	}
+
+	a:hover {
+		color: var(--color-theme-1);
+	}
+</style> -->
+<div class="header">
+    <div class="container">
+        <div>
+            <!-- <ImageWrapper src="/logo.svg" alt="Logo"  /> -->
+            <a href="/">
+				<img src={logo} alt="logo" class="logo"/>
+            </a>
+        </div>
+        <div class="container-header"></div>
+        <!-- <ToggleTheme /> -->
+    </div>
+</div>
+
+<style is:global>
+    .header {
+        box-shadow: 0px 7px 70px rgba(0, 0, 0, 0.04);
+        /* height: 180px; */
+        padding-right: 10px;
+        padding-left: 10px;
+    }
+    .logo {
+        height: 100px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 175px;
+        border-right: 1px solid #ebebeb;
+        border-left: 1px solid #ebebeb;
+        margin-right: 40px;
+        z-index: 1;
+    }
+    img.logo {
+        width: 110px;
+        max-width: 100%;
+        max-height: 100%;
+    }
+    .container-header {
+        padding-left: 35px;
+    }
+    @media (min-width: 1180px) {
+        .container {
+            max-width: 1450px;
+        }
+    }
+    @media (min-width: 1200px) and (max-width: 1730px) {
+        .container {
+            max-width: 100%;
+        }
+    }
+</style>
+
